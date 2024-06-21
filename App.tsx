4368cb2f09c9ba15b79ed70c2811d02f338738f7
Chee Kit <cheekit.chong98@gmail.com>
2024-06-21 15:52:12 +0800

    Add drag and drop example

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { DragDropContentView } from 'expo-drag-drop-content-view';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
+    <SafeAreaView style={styles.container}>
+      <DragDropContentView
+        onDrop={(event) => {
+          alert(`Dropped ${event.assets.length} items`);
+        }}
+        style={styles.container}>
+        <WebView source={{ uri: 'https://www.dragdrop.com/test/' }} style={styles.webView} />
+      </DragDropContentView>
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  webView: { flex: 1 },
+});
